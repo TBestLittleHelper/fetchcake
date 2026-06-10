9aee57a6c748cb3fbd996d1d093e3cefb5482327
TBest <TBestLittleHelper@users.noreply.github.com>
2026-06-10 09:47:27 +0200

    generate template

---
+++ src/slicer/slicer.ts
@@ -0,0 +1,10 @@
+import './slicer.css'
+
+// Initialize slicer page
+const container = document.querySelector<HTMLElement>('#slicerContainer')
+
+if (!container) {
+  throw new Error('Slicer markup is missing from slicer.html.')
+}
+
+// Slicer page functionality can be added here
