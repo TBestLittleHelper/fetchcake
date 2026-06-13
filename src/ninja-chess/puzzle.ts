b495f9192c006492a3a22235e39998a173256d54
TBest <TBestLittleHelper@users.noreply.github.com>
2026-06-13 06:15:27 +0200

    remove startpuzzlegame

--- src/ninja-chess/puzzle.ts
+++ src/ninja-chess/puzzle.ts
@@ -1,15 +1,8 @@
 import samplePuzzleData from '../assets/sample_puzzle.json';
 
-type status = "won" | "inProgress";
-
 export function getPuzzleBatch(startIndex: number) {
 	const puzzles = samplePuzzleData.puzzles.slice(startIndex, startIndex + 30);
 	return puzzles;
-}
-
-export function startPuzzleGame() {
-	let status: status = "inProgress"
-	return status
 }
 
 export function getnbPuzzles() {
