f64fd7fcf50349b31b48f15e629cbcb72bb587bf
TBest <TBestLittleHelper@users.noreply.github.com>
2026-06-17 03:54:06 +0200

    just pick 30 random indexes
    no need to overcomplicate

--- src/ninja-chess/puzzle.ts
+++ src/ninja-chess/puzzle.ts
@@ -3,32 +3,18 @@ import samplePuzzleData from '../assets/sample_puzzle.json';
 const nbPuzzles = 30;
 
 export function getPuzzleBatch(startIndex: number) {
-	let puzzles = samplePuzzleData.puzzles.slice(startIndex, startIndex + nbPuzzles);
-	// Shuffle them, in case someone gets the same index more then once
-	puzzles = shuffle(puzzles)
+	const selected = new Set<number>();
+	const puzzleDatabaseLength = samplePuzzleData.puzzles.length
+
+	while (selected.size < nbPuzzles) {
+		selected.add(Math.floor(Math.random() * puzzleDatabaseLength));
+	}
+	const indices = [...selected];
+	const puzzles = indices.map(index => samplePuzzleData.puzzles[index]);
+
 	return puzzles;
 }
 
 export function getnbPuzzles() {
 	return nbPuzzles;
-}
-
-// https://bost.ocks.org/mike/shuffle/
-// Fisher–Yates shuffle
-function shuffle(array: any[]): any[] {
-	let m = array.length, t, i;
-
-	// While there remain elements to shuffle…
-	while (m) {
-
-		// Pick a remaining element…
-		i = Math.floor(Math.random() * m--);
-
-		// And swap it with the current element.
-		t = array[m];
-		array[m] = array[i];
-		array[i] = t;
-	}
-
-	return array;
 }
